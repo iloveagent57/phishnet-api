8b20517b0d0ed10a63074ff11dbeeeb18bb73b62
Alex Dusenbery <adusen@janysanalytics.com>
2015-07-26 16:43:56 -0400

    First commit

---
+++ pnet/api.py
@@ -0,0 +1,82 @@
+import requests
+
+
+def has_kwargs(*args):
+    allowed_kwargs = set(args)
+    def wrapper(function):
+        def inner(*args, **kwargs):
+            kwargs = {k: v for k, v in kwargs.iteritems() if k in allowed_kwargs}
+            return function(*args, **kwargs)
+        return inner
+    return wrapper
+
+
+class PhishNetApi(object):
+    def __init__(self, api_key, data_format='json'):
+        self.api_key = api_key
+        self.data_format = data_format
+        self._base_url = 'https://api.phish.net/api.js'
+        self._base_params = {
+            'api': '2.0',
+            'format': self.data_format,
+            'apikey': self.api_key
+        }
+
+    def __repr__(self): # pragma: no cover
+        return 'PhishNetApi(api_key={0}, data_format={1})'.format(repr(self.api_key), repr(self.data_format))
+
+    def _get(self, params):
+        payload = self._base_params
+        payload.update(params)
+        response = requests.get(self._base_url, params=payload)
+        if response.status_code != 200:
+            raise Exception(response.status_code)
+
+        data = response.json()
+
+        # Could get a response with status code 200, but some other error could occur,
+        # possibly due to a bad parameter.
+        if isinstance(data, dict) and (data.get('success') == 0):
+            raise Exception(data.get('reason'))
+
+        return data
+
+    def _get_method(self, method_name, params):
+        params = params or {}
+        params.update({'method': method_name})
+        return self._get(params)
+
+    @has_kwargs('year', 'venueid', 'state', 'country', 'month', 'day', 'artist', 'showids')
+    def shows_query(self, **kwargs):
+        """
+        Optional kwargs:
+        :param year: 4 digit year
+        :param venueid: Integer venue id
+        :param state: 2 haracter string
+        :param country: Name of a country
+        :param month: 1 or 2 digit month
+        :param day: 1 or 2 digit day
+        :param artist: Integer artist id (1 is Phish)
+        :param showids: Comma separated string of show ids
+        :returns: A list of shows filtered according to kwargs.
+        """
+        return self._get_method('pnet.shows.query', kwargs)
+
+    def shows_setlist(self, **kwargs):
+        """
+        :param showdate: The date of the show in YYYY-mm-dd format
+        :param showid: The id of the show.  If both ``showdate`` and ``showid`` are specified, showid is used.
+        :returns: A setlist for a given show.
+        """
+        return self._get_method('pnet.shows.setlists.get', kwargs)
+
+    @has_kwargs('username', 'showdate')
+    def reviews_query(self, **kwargs):
+        """
+        Optional kwargs:
+        :param username: If a username is provided, it will return reviews posted by a given user.
+        :param showdate: If a showdate is provided in YYYY-mm-dd format, it will return reviews attached to a given show.
+        If both username and showdate are provided, it will filter by both showdate AND username.
+        :returns: A list of reviews filtered by username, the date of the show, or both.
+        """
+        return self._get_method('pnet.reviews.query', kwargs)
---
+++ tests/test_api.py
@@ -0,0 +1,93 @@
+import unittest
+
+import mock
+
+from pnet.api import has_kwargs, PhishNetApi
+
+
+class TestHasKwargs(unittest.TestCase):
+    @has_kwargs('x', 'y')
+    def foo(self, **kwargs):
+        return kwargs
+
+    def test_has_kwargs_extra_args(self):
+        result = self.foo(x=1, y=2, z=3)
+
+        self.assertEqual({'x', 'y'}, set(result.keys()))
+
+    def test_has_kwargs_no_valid_args(self):
+        result = self.foo(z=3)
+
+        self.assertEqual([], result.keys())
+
+
+class TestPhishNetApi(unittest.TestCase):
+    def setUp(self):
+        self.requests_patcher = mock.patch('pnet.api.requests')
+        self.requests = self.requests_patcher.start()
+        self.api = PhishNetApi('my-key')
+
+    def tearDown(self):
+        self.requests_patcher.stop()
+
+    def test_get_happy_path(self):
+        mock_response = mock.Mock(status_code=200)
+        self.requests.get.return_value = mock_response
+        result = self.api._get({'foo': 5})
+
+        expected_payload = {
+            'api': '2.0',
+            'format': 'json',
+            'apikey': 'my-key',
+            'foo': 5
+        }
+        self.requests.get.assert_called_once_with(self.api._base_url,
+                                                  params=expected_payload)
+        self.assertEqual(result, mock_response.json.return_value)
+
+    def test_get_non_200_status_code(self):
+        self.requests.get.return_value = mock.Mock(status_code=400)
+
+        with self.assertRaisesRegexp(Exception, '400'):
+            self.api._get({'foo': 5})
+
+    def test_get_unsuccessful(self):
+        mock_response = mock.Mock(status_code=200)
+        mock_response.json.return_value = {
+            'success': 0,
+            'reason': 'failure'
+        }
+        self.requests.get.return_value = mock_response
+
+        with self.assertRaisesRegexp(Exception, 'failure'):
+            self.api._get({'foo': 5})
+
+    def test_get_method(self):
+        with mock.patch('pnet.api.PhishNetApi._get') as _get:
+            self.api._get_method('some.endpoint', {'the': 'param'})
+
+            _get.assert_called_once_with({
+                'method': 'some.endpoint',
+                'the': 'param'
+            })
+
+    def test_shows_query(self):
+        with mock.patch('pnet.api.PhishNetApi._get_method', return_value=[{'showid': 1}]) as get_method:
+            result = self.api.shows_query(year=2014)
+
+            self.assertEqual([{'showid': 1}], result)
+            get_method.assert_called_once_with('pnet.shows.query', {'year': 2014})
+
+    def test_shows_setlist(self):
+        with mock.patch('pnet.api.PhishNetApi._get_method', return_value=[{'songs': 'reba'}]) as get_method:
+            result = self.api.shows_setlist(showid=1)
+
+            self.assertEqual([{'songs': 'reba'}], result)
+            get_method.assert_called_once_with('pnet.shows.setlists.get', {'showid': 1})
+
+    def test_reviews_query(self):
+        with mock.patch('pnet.api.PhishNetApi._get_method', return_value=[{'likes': 'yes'}]) as get_method:
+            result = self.api.reviews_query(showdate='2014-12-31')
+
+            self.assertEqual([{'likes': 'yes'}], result)
+            get_method.assert_called_once_with('pnet.reviews.query', {'showdate': '2014-12-31'})
